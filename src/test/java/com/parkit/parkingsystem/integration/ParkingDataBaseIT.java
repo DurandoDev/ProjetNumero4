a52a6fef7574883fe54fdc4de3e610f3d6292d11
DurandoDev <nat.durando@gmail.com>
2021-05-03 11:19:01 +0200

    Integration test testParkingACar done.

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,9 +1,11 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.ParkingSpot;
 import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
@@ -61,10 +63,17 @@ public class ParkingDataBaseIT {
     @Test
     public void testParkingACar(){
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+
+        int parkACar1 = parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR);
+
         parkingService.processIncomingVehicle();
 
+        int parkACar2 = parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR);
+
         Ticket ticket = ticketDAO.getTicket("ABCDEF");
+
         assertNotNull(ticket);
+        assertEquals(parkACar1+1,parkACar2);
 
         //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
     }
